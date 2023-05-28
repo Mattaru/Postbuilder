fc22687ea7e084f3ba9374385cf94c8720a56a4e
Maksim Smolnikau <gctdljgkjnm@mail.ru>
2023-05-28 11:18:21 +0300

    Maked CheckBox style and added coaple commands

--- PBapp/PBapp/MVVM/ViewModels/MainViewModel.cs
+++ PBapp/PBapp/MVVM/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using PBapp.Core;
 using PBapp.Infrastructure.Commands;
 using System;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
 
@@ -13,6 +14,58 @@ namespace PBapp.MVVM.ViewModels
         private string _title = "";
 
         public string Title { get => _title; set => Set(ref _title, value); }
+
+        #endregion
+
+        private ObservableCollection<string> _ingredients;
+
+        public ObservableCollection<string> Ingredients
+        {
+            get => _ingredients;
+
+            set => Set(ref _ingredients, value);
+        }
+
+        // Commands
+
+        #region AddIngredient
+
+        public ICommand AddIngredient { get;  }
+
+        private bool CanAddIngredientCommandExecute(object p) => true;
+
+        private void OnAddIngredientCommandExecuted(object p)
+        {
+            var values = (object[])p;
+
+            string ingredient = (string)values[0];
+
+            bool check = (bool)values[1];
+
+            if (check)
+            {
+                Ingredients.Add(ingredient);
+            }
+            else
+            {
+                Ingredients.Remove(ingredient);
+            }
+
+        }
+
+        #endregion
+
+        #region CopyToClipBoard
+
+        public ICommand CopyToClipBoard { get; }
+
+        private bool CanCopyToClipBoardCommandExecute(object p) => true;
+
+        private void OnCopyToClipBoardCommandExecuted(object p)
+        {
+            
+
+        }
 
         #endregion
 
